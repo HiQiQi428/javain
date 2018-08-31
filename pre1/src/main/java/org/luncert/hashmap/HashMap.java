75ce89625d20f2d2a23f4da8b826b77c86eba694
HiQiQi428 <2725115515@qq.com>
2018-08-31 15:14:27 +0800

    ..

--- pre1/src/main/java/org/luncert/hashmap/HashMap.java
+++ pre1/src/main/java/org/luncert/hashmap/HashMap.java
@@ -7,28 +7,48 @@ import java.util.Set;
 public class HashMap<K,V> implements Map<K,V> {
 
 	static int DEFAULT_CAPACITY = 1 << 4;
-	static float DEFAULT_THRESHOLD = 0.75f;
+	static float DEFAULT_LOADFACTOR = 0.75f;
 
-	Node<K,V>[] data;
+	Node<K,V>[] table;
 	int capacity = DEFAULT_CAPACITY;
-	float threshold = DEFAULT_THRESHOLD;
+	float loadFactor = DEFAULT_LOADFACTOR;
+	int threshold = (int) (capacity * loadFactor);
+	int size = 0;
 	
 	private int hash(K key) {
-		if (key == null)
-			return 0;
-		else {
-			int h = key.hashCode();
-			h &= h >>> 16;
-			return h;
-		}
+		int h;
+		return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
 	}
 
-	private void putVal(int hash, K key, V value) {
-
-	}
-
+	@SuppressWarnings("unchecked")
 	private void resize() {
-
+		if (table == null || table.length == 0)
+			table = (Node<K,V>[]) new Node[capacity];
+		else {
+			capacity <<= 1;
+			threshold <<= 1;
+			Node<K,V>[] oldTable = table;
+			table = (Node<K,V>[]) new Node[capacity];
+			// 拷贝元素
+			for (int i = 0; i < oldTable.length; i++) {
+				Node<K,V> tmp = oldTable[i];
+				if (oldTable[i] != null) {
+					oldTable[i] = null;
+					for (; tmp != null; tmp = tmp.next) {
+						int hash = tmp.hash;
+						int index = (capacity - 1) & hash;
+						if (table[index] == null)
+							table[index] = tmp;
+						else {
+							Node<K,V> e = table[index];
+							while (e.next != null)
+								e = e.next;
+							e.next = tmp;
+						}
+					}
+				}
+			}
+		}
 	}
 
     static class Node<K,V> implements Map.Entry<K,V> {
@@ -58,18 +78,18 @@ public class HashMap<K,V> implements Map<K,V> {
 			return oldValue;
 		}
 
-	}
-	
-	@SuppressWarnings("unchecked")
-	HashMap() {
-		data = new Node[capacity];
+		@Override
+		public String toString() {
+			return key + "=" + value;
+		}
+
 	}
 
 	@Override
-	public int size() { return data.length; }
+	public int size() { return size; }
 
 	@Override
-	public boolean isEmpty() { return size() == 0; }
+	public boolean isEmpty() { return size == 0; }
 
 	@Override
 	public boolean containsKey(Object key) {
@@ -88,9 +108,26 @@ public class HashMap<K,V> implements Map<K,V> {
 
 	@Override
 	public V put(K key, V value) {
-		putVal(hash(key), key, value);
-		if (size() > threshold * capacity)
+		if (table == null || table.length == 0 || size + 1 >= threshold)
 			resize();
+		int hash = hash(key);
+		int index = (capacity - 1) & hash;
+		if (table[index] == null)
+			table[index] = new Node<K, V>(hash, key, value, null);
+		else {
+			Node<K,V> node = table[index], cur, next = node;
+			do {
+				cur = next;
+				if ((cur.hash == hash) && cur.key.equals(key)) {
+					V oldValue = cur.value;
+					cur.value = value;
+					return oldValue;
+				}
+			}
+			while ((next = cur.next) != null);
+			cur.next = new Node<K, V>(hash, key, value, null);
+		}
+		size++;
 		return value;
 	}
 
@@ -122,6 +159,15 @@ public class HashMap<K,V> implements Map<K,V> {
 	@Override
 	public Set<Entry<K, V>> entrySet() {
 		return null;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < table.length && table[i] != null; i++)
+			for (Node<K,V> tmp = table[i]; tmp != null; tmp = tmp.next)
+				builder.append(tmp).append('\n');
+		return builder.toString();
 	}
     
 }
--- pre1/src/test/java/org/luncert/TestHashMap.java
+++ pre1/src/test/java/org/luncert/TestHashMap.java
@@ -1,11 +1,11 @@
 package org.luncert;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.luncert.hashmap.HashMap;
 
 @RunWith(JUnit4.class)
 public class TestHashMap {
@@ -13,13 +13,11 @@ public class TestHashMap {
     @Test
     public void test() {
         Map<Integer, String> map = new HashMap<>();
-        map.put(17, "17");
-        System.out.println(map.get(1));
-        // for (int i = 0; i < 32; i++) {
-        //     if (i == 17)
-        //         continue;
-        //     map.put(i, String.valueOf(i));
-        // }
+        for (int i = 0; i < 16; i ++)
+            map.put(new Integer(i), String.valueOf(i));
+        for (int i = 0; i < 16; i ++)
+            map.put(new Integer(i + 16), String.valueOf(i + 16));
+        System.out.println(map.toString());
     }
     
 }
